03edf888461005eb60bd29eb325559f88ec8efb0
Aaron VonderHaar <gruen0aermel@gmail.com>
2009-04-25 23:35:20 -0700

    Tried adding jbehave to the maven project.

---
+++ Test1/src/test/java/net/avh4/scratch/Test1/AppScenario.java
@@ -0,0 +1,12 @@
+package net.avh4.scratch.Test1;
+
+import org.jbehave.scenario.Scenario;
+
+public class AppScenario extends Scenario {
+
+    public AppScenario()
+    {
+        super(new CommonSteps());
+    }
+
+}
---
+++ Test1/src/test/java/net/avh4/scratch/Test1/CommonSteps.java
@@ -0,0 +1,7 @@
+package net.avh4.scratch.Test1;
+
+import org.jbehave.scenario.steps.Steps;
+
+class CommonSteps extends Steps {
+
+}
